0127ab4dddfb88b1b093a66198b28d7f0acd56cd
pabironseizin <138739459+pabironseizin@users.noreply.github.com>
2023-07-14 13:13:12 +0900

    appの追加
    appのプログラムを３つつくった

---
+++ yanikasu/yanikasu/GameMainScene.cpp
@@ -0,0 +1,13 @@
+#include"GameMainScene.h"
+#include"DxLib.h"
+#include"Stage.h"
+#include"SceneManager.h"
+
+/****************************************
+
+*ƒ}ƒNƒ’è‹`
+
+****************************************/
+
+#define TIMELIMIT    (3600*3)  //§ŒÀŠÔ
+#define NUMBER_IMAGE_MAX (10)  //”š‰æ‘œ”
