99d1ee8ed6592dbb8fa2f50daaf2d74b74116297
pabironseizin <138739459+pabironseizin@users.noreply.github.com>
2023-07-21 10:27:10 +0900

    GameMainScene.app完成
    GameMainScene.appの書き込みが終わった

--- yanikasu/yanikasu/GameMainScene.cpp
+++ yanikasu/yanikasu/GameMainScene.cpp
@@ -117,5 +117,61 @@ void GameMainScene_Update(void)
 	//制限時間の更新
 	GameTime--;
 
-	//
+	//制限時間が無くなったら、ゲームオーバーに移動する
+	if (GameTime < 0)
+	{
+		Change_Scene(E_GAME_OVER);
+	}
+
+	//ミッションを達成したら、ゲームクリアに移動する
+	if (Get_StageClearFlag())
+	{
+		Change_Scene(E_GAME_CLEAR);
+	}
+
+}
+
+/****************************************
+
+*ゲームメイン画面：描画処理
+
+* 引　数：なし
+
+*戻り値：なし
+
+****************************************/
+
+void GameMainScene_Draw(void)
+{
+	int PosX = 600;
+	int tmp_level = GameLevel;
+	int tmp_score = Get_StageScore();
+
+	//ステージを描画
+	StageDraw();
+
+	//フェードアウト状態か？
+	if (Get_StageState() == 1)
+	{
+		FadeOutBlock();  //フェードアウトする
+	}
+	//レベルを描画
+	do
+	{
+		DrawRotaGraph(PosX, 80, 0.5f, 0, NumberImage[tmp_level % 10], TRUE);
+		tmp_level /= 10;
+		PosX -= 30;
+	} while (tmp_level > 0);
+
+	//スコアの描画
+	PosX = 620;
+	do {
+		DrawRotaGraph(PosX, 160, 0.3, 0, NumberImage[tmp_score % 10], TRUE);
+		tmp_score /= 10;
+		PosX -= 20;
+	} while (tmp_score > 0);
+
+	//制限時間の描画
+	DrawBox(491, 469, 509, 469 - GameTime / 60 * 2, 0x0033ff, TRUE);
+
 }
