7c9963e6ca18ec96cd12a11390915df2258cb667
pabironseizin <138739459+pabironseizin@users.noreply.github.com>
2023-08-09 12:11:39 +0900

    stage.cpp書き込み
    stage.cppの書き込み途中

--- yanikasu/yanikasu/Stage.cpp
+++ yanikasu/yanikasu/Stage.cpp
@@ -84,7 +84,7 @@ void restore_block(void);
 
 * 引　数：なし
 
-*戻り値：なエラー情報
+*戻り値：エラー情報
 
 ****************************************/
 int StageInitialize(void)
@@ -126,5 +126,123 @@ int StageInitialize(void)
 		}
 	}
 
+	if (StageImage == -1)
+	{
+		ret = -1;
+	}
+
+	if (ClickSE == -1)
+	{
+		ret = -1;
+	}
+
+	if (FadeOutSE == -1)
+	{
+		ret = -1;
+	}
+
+	if (MoveBlockSE == -1)
+	{
+		ret = -1;
+	}
+
+	return ret;
+}
+
+/****************************************
+
+*ステージ制御機能：ステージの描画
+
+* 引　数：なし
+
+*戻り値：なし
+
+****************************************/
+void StageDraw(void)
+{
+	DrawGraph(0, 0, StageImage, FALSE);
+
+	//アイテムの取得個数を描画
+	for (int i = 0; i < ITEM_MAX; i++)
+	{
+		DrawRotaGraph(540, 245 + i * 30, 0.5f, 0, BlockImage[i + 1], TRUE, 0);
+		DrawFormatString(580, 235 + i * 30, 0xffffff, "%3d", Item[i]);
+	}
+
+	//ブロックを描画
+	for (int i = 0; i < HEIGHT; i++)
+	{
+		for (int j = 0; j < WIDTH; j++)
+		{
+			if (Block[i][j].flg == TRUE && Block[i][j].image != NULL)
+			{
+				DrawGraph(Block[i][j].x, Block[i][j].y, BlockImage[Block[i][j].image], TRUE);
+			}
+		}
+	}
+
+	//選択ブロックを描画
+	DrawGraph(Select[SELECT_CURSOR].x * BLOCKSIZE, Select[SELECT_CURSOR].y * BLOCKSIZE, BlockImage[9], TRUE);
+	if (ClickStatus != E_NONE)
+	{
+		DrawGraph(Select[NEXT_CURSOR].x * BLOCKSIZE, Select[NEXT_CURSOR].y * BLOCKSIZE, BlockImage[9], TRUE);
+	}
+
+	//ミッションを描画
+	SetFontSize(20);
+	DrawFormatString(590, 211, GetColor(255, 255, 255), "%3d", Stage_Mission);
 
 }
+
+/****************************************
+
+*ステージ制御機能：ブロック生成処理
+
+* 引　数：なし
+
+*戻り値：なし
+
+****************************************/
+void CreateBlock(void)
+{
+	int Check = 0;
+	int i, j;
+
+	do
+	{
+		Check = 0;
+		for (i = 0; i < HEIGHT; i++)
+		{
+			for (j = 0; j < WIDTH; j++)
+			{
+				if (j == 0 || j == WIDTH - 1 || i == HEIGHT - 1 || i == 0)
+				{
+					Block[i][j].flg = FALSE;
+					Block[i][j].image = NULL;
+				}
+				else
+				{
+					Block[i][j].flg = TRUE;
+					Block[i][j].x = (j - 1) * BLOCKSIZE;
+					Block[i][j].y = (i - 1) * BLOCKSIZE;
+					Block[i][j].width = BLOCKSIZE;
+					Block[i][j].height = BLOCKSIZE;
+					Block[i][j].image = GetRand(7) + 1; //1～8の乱数
+				}
+			}
+		}
+
+	/*for (i = 1; i<HEIGHT - 1; i++)
+	{
+		for (j = 1; j < WIDTH - 1; j++)
+		{
+			if (Block[i][j].image == NULL)
+			{
+				Block[i][j].image = GetRand(7) + 1;
+			}
+		}
+	}*/
+		
+	//ブロック連鎖チェック
+}
+
